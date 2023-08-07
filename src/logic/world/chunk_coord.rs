9aed0d1bf5e766818c6684c331bc68e8fc8f8685
Vladislav Sumin <vladislavsumin@mail.ru>
2023-08-07 19:00:49 +0300

    fix chunk coord

--- src/logic/world/chunk_coord.rs
+++ src/logic/world/chunk_coord.rs
@@ -5,31 +5,31 @@ use crate::logic::chunk::CHUNK_SIZE;
 /// **Note** Например, второй чанк по оси Х, будет иметь координату X = 1, а не 16!
 #[derive(Default)]
 pub struct ChunkCoord {
-    pub x: u32,
-    pub y: u32,
-    pub z: u32,
+    pub x: i32,
+    pub y: i32,
+    pub z: i32,
 }
 
 impl ChunkCoord {
-    pub fn new(x: u32, y: u32, z: u32) -> Self {
+    pub fn new(x: i32, y: i32, z: i32) -> Self {
         Self { x, y, z }
     }
 
     /// Возвращает координаты чанка внутри которого находятся [coord]
     pub fn from_global_coord(coord: Vec3) -> Self {
         ChunkCoord::new(
-            (coord.x as u32) / (CHUNK_SIZE as u32),
-            (coord.y as u32) / (CHUNK_SIZE as u32),
-            (coord.z as u32) / (CHUNK_SIZE as u32),
+            (coord.x as i32) / (CHUNK_SIZE as i32),
+            (coord.y as i32) / (CHUNK_SIZE as i32),
+            (coord.z as i32) / (CHUNK_SIZE as i32),
         )
     }
 
     /// Возвращает абсолютные координаты чанка в мире
     pub fn get_absolute_coord(&self) -> Vec3 {
         vec3(
-            (self.x * CHUNK_SIZE as u32) as f32,
-            (self.y * CHUNK_SIZE as u32) as f32,
-            (self.z * CHUNK_SIZE as u32) as f32,
+            (self.x * CHUNK_SIZE as i32) as f32,
+            (self.y * CHUNK_SIZE as i32) as f32,
+            (self.z * CHUNK_SIZE as i32) as f32,
         )
     }
 }
