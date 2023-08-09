9deb6cc2fcbf182adf2f5bd878aecb4d8d38259c
Vladislav Sumin <vladislavsumin@mail.ru>
2023-08-09 18:57:30 +0300

    add doc

--- feature/chunk/src/chunk_map.rs
+++ feature/chunk/src/chunk_map.rs
@@ -3,6 +3,7 @@ use std::ops::{Deref, DerefMut};
 use generic_assert::{Assert, IsTrue};
 use crate::{Chunk, ChunkPos};
 
+/// Карта с чанками
 #[derive(Default)]
 pub struct ChunkMap<const CHUNK_SIZE: usize, BLOCK, METADATA> where Assert<{ CHUNK_SIZE <= u8::MAX as usize }>: IsTrue {
     chunks: HashMap<ChunkPos<CHUNK_SIZE>, Chunk<CHUNK_SIZE, BLOCK, METADATA>>,
