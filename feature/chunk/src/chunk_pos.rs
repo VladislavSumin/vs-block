a3989eacfd798a5eb5474f6da4252369f155bef5
Vladislav Sumin <vladislavsumin@mail.ru>
2023-08-10 20:34:48 +0300

    add casts form chunkPos

--- feature/chunk/src/chunk_pos.rs
+++ feature/chunk/src/chunk_pos.rs
@@ -1,6 +1,8 @@
-use std::ops::Deref;
-use bevy_math::{IVec3, ivec3, Vec3};
+use std::ops::{Add, Deref, DerefMut};
+use bevy_math::{IVec3, ivec3};
 use generic_assert::{Assert, IsTrue};
+use crate::absolute_block_pos::AbsoluteBlockPos;
+use crate::ChunkBlockPos;
 
 /// Координаты чанка в сетке чанков.
 /// **Note** Например, второй чанк по оси Х, будет иметь координату X = 1, а не CHUNK_SIZE!
@@ -12,8 +14,7 @@ pub struct ChunkPos<const CHUNK_SIZE: usize> where Assert<{ CHUNK_SIZE <= u8::MA
 
 impl<const CHUNK_SIZE: usize> ChunkPos<CHUNK_SIZE> where Assert<{ CHUNK_SIZE <= u8::MAX as usize }>: IsTrue {
     /// Возвращает координаты чанка внутри которого находятся [coord]
-    pub fn from_global_coord(coord: Vec3) -> Self {
-        let coord = coord.as_ivec3();
+    pub fn from_global_coord(coord: IVec3) -> Self {
         ivec3(
             (coord.x + (coord.x < 0) as i32) / CHUNK_SIZE as i32 - (coord.x < 0) as i32,
             (coord.y + (coord.y < 0) as i32) / CHUNK_SIZE as i32 - (coord.y < 0) as i32,
@@ -22,8 +23,8 @@ impl<const CHUNK_SIZE: usize> ChunkPos<CHUNK_SIZE> where Assert<{ CHUNK_SIZE <= 
     }
 
     /// Возвращает абсолютные координаты чанка в мире
-    pub fn get_absolute_coord(&self) -> Vec3 {
-        (self.pos * CHUNK_SIZE as i32).as_vec3()
+    pub fn get_absolute_coord(&self) -> IVec3 {
+        self.pos * CHUNK_SIZE as i32
     }
 
     pub fn raw_pos(&self) -> &IVec3 {
@@ -43,6 +44,12 @@ impl<const CHUNK_SIZE: usize> Deref for ChunkPos<CHUNK_SIZE> where Assert<{ CHUN
     }
 }
 
+impl<const CHUNK_SIZE: usize> DerefMut for ChunkPos<CHUNK_SIZE> where Assert<{ CHUNK_SIZE <= u8::MAX as usize }>: IsTrue {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.pos
+    }
+}
+
 impl<const CHUNK_SIZE: usize> From<IVec3> for ChunkPos<CHUNK_SIZE>
     where Assert<{ CHUNK_SIZE <= u8::MAX as usize }>: IsTrue {
     fn from(value: IVec3) -> Self {
@@ -50,16 +57,35 @@ impl<const CHUNK_SIZE: usize> From<IVec3> for ChunkPos<CHUNK_SIZE>
     }
 }
 
+/// Сумма [ChunkPos] + [ChunkBlockPos] = [AbsoluteBlockPos]
+impl<const CHUNK_SIZE: usize> Add<ChunkBlockPos<CHUNK_SIZE>> for ChunkPos<CHUNK_SIZE>
+    where Assert<{ CHUNK_SIZE <= u8::MAX as usize }>: IsTrue {
+    type Output = AbsoluteBlockPos;
+
+    fn add(self, rhs: ChunkBlockPos<CHUNK_SIZE>) -> Self::Output {
+        (self.get_absolute_coord() + rhs.as_ivec3()).into()
+    }
+}
+
+/// [ChunkPos] из [AbsoluteBlockPos]
+/// Позиция чанка в котором находится блок
+impl<const CHUNK_SIZE: usize> From<AbsoluteBlockPos> for ChunkPos<CHUNK_SIZE>
+    where Assert<{ CHUNK_SIZE <= u8::MAX as usize }>: IsTrue {
+    fn from(value: AbsoluteBlockPos) -> Self {
+        ChunkPos::from_global_coord(value.into())
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use bevy_math::vec3;
+    use bevy_math::{ivec3, vec3};
     use crate::chunk_pos::ChunkPos as ChunkCoordTemplate;
 
     type ChunkCoord = ChunkCoordTemplate<16>;
 
     #[test]
     fn from_global_coord_min_coord() {
-        let coord = ChunkCoord::from_global_coord(vec3(0., 0., 0.));
+        let coord = ChunkCoord::from_global_coord(ivec3(0, 0, 0));
         assert_eq!(coord.pos.x, 0);
         assert_eq!(coord.pos.y, 0);
         assert_eq!(coord.pos.z, 0);
@@ -67,7 +93,7 @@ mod tests {
 
     #[test]
     fn from_global_coord_inside_chunk_coord() {
-        let coord = ChunkCoord::from_global_coord(vec3(1., 2., 15.99));
+        let coord = ChunkCoord::from_global_coord(ivec3(1, 2, 15));
         assert_eq!(coord.pos.x, 0);
         assert_eq!(coord.pos.y, 0);
         assert_eq!(coord.pos.z, 0);
@@ -75,7 +101,7 @@ mod tests {
 
     #[test]
     fn from_global_coord_second_chunk() {
-        let coord = ChunkCoord::from_global_coord(vec3(18., 19., 22.));
+        let coord = ChunkCoord::from_global_coord(ivec3(18, 19, 22));
         assert_eq!(coord.pos.x, 1);
         assert_eq!(coord.pos.y, 1);
         assert_eq!(coord.pos.z, 1);
@@ -83,7 +109,7 @@ mod tests {
 
     #[test]
     fn from_global_coord_negative_coord() {
-        let coord = ChunkCoord::from_global_coord(vec3(-1., -2., 2.));
+        let coord = ChunkCoord::from_global_coord(ivec3(-1, -2, 2));
         assert_eq!(coord.pos.x, -1);
         assert_eq!(coord.pos.y, -1);
         assert_eq!(coord.pos.z, 0);
