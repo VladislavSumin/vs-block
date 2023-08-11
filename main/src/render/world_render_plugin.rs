030adca1f02241bf093b85fead378be8e1ad42c5
Vladislav Sumin <vladislavsumin@mail.ru>
2023-08-12 01:12:27 +0300

    move material creation outside from chunk render

--- main/src/render/world_render_plugin.rs
+++ main/src/render/world_render_plugin.rs
@@ -3,10 +3,7 @@ use bevy::asset::Assets;
 use bevy::pbr::{PbrBundle, StandardMaterial};
 use bevy::prelude::*;
 use bevy::utils::HashMap;
-use strum::IntoEnumIterator;
-use chunk::{AbsoluteBlockPos, ChunkBlockPos, ChunkPos};
-use crate::logic::chunk::{Chunk, ChunkMap};
-use crate::render::{AbsoluteBlockFaceDirection, MeshBuilder};
+use chunk::ChunkPos;
 use crate::logic::world::{ChunkUpdateEvent, World};
 use crate::render::chunk_mesh_builder::build_chunk_mesh;
 
@@ -23,8 +20,39 @@ impl Plugin for ChunkRenderPlugin {
     fn build(&self, app: &mut App) {
         app
             .init_resource::<RenderedChunksMap>()
-            .add_systems(Update, update_chunk_mesh);
+            .add_systems(Startup, load_world_material)
+            .add_systems(Update, update_chunk_mesh)
+        ;
     }
+}
+
+#[derive(Resource)]
+struct WorldMaterial {
+    material_handle: Handle<StandardMaterial>,
+}
+
+fn load_world_material(
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    asset_server: Res<AssetServer>,
+    mut commands: Commands,
+) {
+    let texture: Handle<Image> = asset_server.load("dirt.png");
+
+    let material = StandardMaterial {
+        base_color_texture: Some(texture),
+        unlit: false,
+        metallic: 0.,
+        reflectance: 0.,
+        ..default()
+    };
+
+    let material_handle = materials.add(material);
+    //
+    let world_material = WorldMaterial {
+        material_handle
+    };
+
+    commands.insert_resource(world_material);
 }
 
 // TODO тестовая таска, переписать полностью
@@ -33,9 +61,8 @@ fn update_chunk_mesh(
     mut chunk_event: EventReader<ChunkUpdateEvent>,
     world: Res<World>,
     mut assets: ResMut<Assets<Mesh>>,
-    asset_server: Res<AssetServer>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
     mut rendered_chunks_map: ResMut<RenderedChunksMap>,
+    world_material: Res<WorldMaterial>,
 ) {
     for event in chunk_event.iter() {
         match event {
@@ -43,22 +70,19 @@ fn update_chunk_mesh(
                 rendered_chunks_map.chunks.insert(*pos, *entity);
 
                 let chunk = world.get_chunk(pos).unwrap();
-                let mesh = assets.add(build_chunk_mesh(chunk, *pos));
+                let mesh = build_chunk_mesh(chunk, *pos);
 
-                let texture: Handle<Image> = asset_server.load("dirt.png");
+                // Не спавним пустые меши, это сильно бьет по производительности рендера
+                if mesh.indices().map(|indexes| { indexes.is_empty() }).unwrap_or(true) {
+                    continue;
+                }
 
-                let material = StandardMaterial {
-                    base_color_texture: Some(texture),
-                    unlit: false,
-                    metallic: 0.,
-                    reflectance: 0.,
-                    ..default()
-                };
+                let mesh = assets.add(mesh);
 
                 commands.entity(*entity).insert(
                     PbrBundle {
                         mesh,
-                        material: materials.add(material),
+                        material: world_material.material_handle.clone(),
                         transform: Transform::from_translation(pos.get_absolute_coord().as_vec3()),
                         ..default()
                     }
