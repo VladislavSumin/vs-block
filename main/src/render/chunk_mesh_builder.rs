60ad36b5491fadd18296aac44cb9f85f02f0683f
Vladislav Sumin <vladislavsumin@mail.ru>
2023-08-14 18:55:36 +0300

    add more doc

--- main/src/render/chunk_mesh_builder.rs
+++ main/src/render/chunk_mesh_builder.rs
@@ -8,12 +8,18 @@ use crate::render::{AbsoluteBlockFaceDirection, MeshBuilder};
 /// Строит [Mesh] для чанка
 pub fn build_chunk_mesh(chunk_map: &ChunkMap, chunk: &Chunk, chunk_pos: ChunkPos) -> Mesh {
     let mut builder = MeshBuilder::new();
-    for (block_coord, block) in chunk.into_iter() {
-        if let Some(_) = block {
-            builder.set_transition(block_coord.into());
 
+    // Итерируемся по всем блокам
+    for (block_pos, block) in chunk.into_iter() {
+        if let Some(_) = block {
+            // Устанавливаем координаты блока в блидер (теперь добавленные меши будут автоматически
+            // сдвинуты на эту величину
+            builder.set_transition(block_pos.into());
+
+            // Проходимся по всем граням блока
             for dir in AbsoluteBlockFaceDirection::iter() {
-                if !has_neighbor_block(chunk_map, chunk, chunk_pos, block_coord, dir) {
+                // Если блок не имеет соседа со стороны проверяемой грани, то добавлем эту грань в меш
+                if !has_neighbor_block(chunk_map, chunk, chunk_pos, block_pos, dir) {
                     builder.add_mesh_data(dir);
                 }
             }
@@ -22,11 +28,21 @@ pub fn build_chunk_mesh(chunk_map: &ChunkMap, chunk: &Chunk, chunk_pos: ChunkPos
     builder.build()
 }
 
-fn has_neighbor_block(chunk_map: &ChunkMap, chunk: &Chunk, chunk_pos: ChunkPos, pos: ChunkBlockPos, dir: AbsoluteBlockFaceDirection) -> bool {
-    if chunk_pos.z == 0 && pos.z == 0 { return true; }
+/// Возвращает имеется ли блок со стороны переданной грани блока
+fn has_neighbor_block(
+    chunk_map: &ChunkMap,
+    chunk: &Chunk,
+    chunk_pos: ChunkPos,
+    block_pos: ChunkBlockPos,
+    dir: AbsoluteBlockFaceDirection,
+) -> bool {
+    // Если это самый нижний блок (дно мира) то снизу у него "есть" соседи, это нужно для отсечения нижних граней
+    // потому что снизу на мир никто смотреть не должен
+    if chunk_pos.z == 0 && block_pos.z == 0 { return true; }
+
     // Вычисляем глобальные координаты блока соседствующего с данным с переданной стороны
     let dir_pos: IVec3 = dir.into();
-    let global_pos: AbsoluteBlockPos = (chunk_pos.get_absolute_coord() + pos.as_ivec3() + dir_pos).into();
+    let global_pos: AbsoluteBlockPos = (chunk_pos.get_absolute_coord() + block_pos.as_ivec3() + dir_pos).into();
 
     if let Ok(local_pos) = chunk_pos.try_global_pos_into_chunk_pos(global_pos) {
         chunk[&local_pos].is_some()
