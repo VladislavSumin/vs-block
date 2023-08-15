c372b7e39495bed45f2f89819b5f065fefdcd7a1
Vladislav Sumin <vladislavsumin@mail.ru>
2023-08-15 22:06:32 +0300

    fix render chunk walls

--- main/src/render/chunk_mesh_builder.rs
+++ main/src/render/chunk_mesh_builder.rs
@@ -11,14 +11,14 @@ pub fn build_chunk_mesh(chunk_map: &ChunkMap, chunk: &Chunk, chunk_pos: ChunkPos
     // Итерируемся по всем блокам
     for (block_pos, block) in chunk.into_iter() {
         if let Some(_) = block {
-            // Устанавливаем координаты блока в блидер (теперь добавленные меши будут автоматически
+            // Устанавливаем координаты блока в билдер (теперь добавленные меши будут автоматически
             // сдвинуты на эту величину
             builder.set_transition(block_pos.into());
 
             // Проходимся по всем граням блока
             for face_dir in AbsoluteBlockFaceDirection::iter() {
                 // Если блок не имеет соседа со стороны проверяемой грани, то добавляем эту грань в меш
-                if !has_neighbor_block(chunk_map, chunk, chunk_pos, block_pos, face_dir) {
+                if !is_need_to_render_face(chunk_map, chunk, chunk_pos, block_pos, face_dir) {
                     builder.add_mesh_data(face_dir);
                 }
             }
@@ -27,24 +27,33 @@ pub fn build_chunk_mesh(chunk_map: &ChunkMap, chunk: &Chunk, chunk_pos: ChunkPos
     builder.build()
 }
 
-/// Возвращает имеется ли блок со стороны переданной грани блока
-fn has_neighbor_block(
+/// Возвращает нужно ли рендерить данную грань блока
+fn is_need_to_render_face(
     chunk_map: &ChunkMap,
     chunk: &Chunk,
     chunk_pos: ChunkPos,
     block_pos: ChunkBlockPos,
     face_dir: AbsoluteBlockFaceDirection,
 ) -> bool {
-    // Если это самый нижний блок (дно мира) то снизу у него "есть" соседи, это нужно для отсечения нижних граней
-    // потому что снизу на мир никто смотреть не должен
-    if chunk_pos.z == 0 && block_pos.z == 0 { return true; }
-
     // Вычисляем глобальные координаты блока соседствующего с данным с переданной стороны
     let global_pos: AbsoluteBlockPos = chunk_pos + block_pos + face_dir;
 
     if let Ok(local_pos) = chunk_pos.try_global_pos_into_chunk_pos(global_pos) {
+        // Соседний блок находится в текущем чанке
         chunk[&local_pos].is_some()
     } else {
-        chunk_map.get_block_at(global_pos).is_some()
+        // Соседний блок находится в соседнем чанке
+        let neighbor_chunk_pos = ChunkPos::from_global_coord(*global_pos);
+        let neighbor_chunk = chunk_map.get(&neighbor_chunk_pos);
+        match neighbor_chunk {
+            None => {
+                // Если соседний чанк не загружен то не рендерим грани блоков обращенных к нему
+                true
+            }
+            Some(chunk) => {
+                let block_pos = neighbor_chunk_pos.try_global_pos_into_chunk_pos(global_pos).unwrap();
+                chunk[&block_pos].is_some()
+            }
+        }
     }
 }
